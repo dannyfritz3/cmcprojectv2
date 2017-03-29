6ae930d9cebda9c0b21647a8c3d5f527fcd267a3
Ethan Ferry <nullpointermisconception@gmail.com>
2017-03-28 19:10:43 -0500

    Pushing LCTest

---
+++ tests/controllers/LogonControllerTest.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LogonControllerTest {
+
+	@Test
+	public void testLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLogout() {
+		fail("Not yet implemented");
+	}
+
+}
