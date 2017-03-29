15a5cbfb36ea82bada46b396bd6ccc30c37047d0
mjzent <mjzent@viewlincsci06.ad.csbsju.edu>
2017-03-28 19:14:12 -0500

    search controller test

---
+++ tests/controllers/SearchControllerTest.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SearchControllerTest {
+
+	@Test
+	public void testFindRelatedUniversities() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUniversity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSearchedUniversities() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+
+}
