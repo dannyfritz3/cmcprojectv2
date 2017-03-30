b4a82cbb7327cc1e321e5b6f262260c85275e048
mjzent <mjzent@viewlincsci38.ad.csbsju.edu>
2017-03-30 18:15:14 -0500

    84 tests later

--- tests/controllers/SearchControllerTest.java
+++ tests/controllers/SearchControllerTest.java
@@ -2,6 +2,8 @@ package controllers;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -12,12 +14,27 @@ import baseclasses.University;
 public class SearchControllerTest {
 	SearchController search;
 	static University test;
-	
+	static University test1;
+	static University test2;
+	static University test3;
+	static University test4;
+	static University test5;
 	@BeforeClass
 	public static void setup(){
 		DBController.getMaxMinValues();
 		test = new University("TEST", "MINNESOTA", "URBAN", "PRIVATE", 5000, 50, 250, 250, 10000, 80, 2500, 60, 90, 2, 2, 2, null);
+		test1 = new University("TEST1", "MINNESOTA", "URBAN", "PRIVATE", 5000, 50, 250, 250, 10000, 80, 2500, 60, 90, 1, 2, 2, null);
+		test2 = new University("TEST2", "MINNESOTA", "URBAN", "PRIVATE", 5000, 50, 250, 250, 50000, 80, 2500, 60, 90, 2, 2, 2, null);
+		test3 = new University("TEST3", "MINNESOTA", "URBAN", "PRIVATE", 5000, 50, 250, 250, 10000, 10, 2500, 60, 90, 2, 2, 2, null);
+		test4 = new University("TEST4", "MINNESOTA", "SUBURBAN", "PRIVATE", 5000, 50, 250, 250, 10000, 80, 2500, 60, 90, 2, 2, 2, null);
+		test5 = new University("TEST5", "MINNESOTA", "URBAN", "PRIVATE", 5001, 51, 251, 251, 10000, 80, 2500, 60, 90, 2, 2, 2, null);
+
 		DBController.addUniversity(test);
+		DBController.addUniversity(test1);
+		DBController.addUniversity(test2);
+		DBController.addUniversity(test3);
+		DBController.addUniversity(test4);
+		DBController.addUniversity(test5);
 		for(University uni : DBController.getUniversities()){
 			System.out.println("\t" + uni.getName());
 		}
@@ -31,13 +48,34 @@ public class SearchControllerTest {
 	@AfterClass
 	public static void teardown(){
 		DBController.deleteSchool("TEST");
+		DBController.deleteSchool("TEST1");
+		DBController.deleteSchool("TEST2");
+		DBController.deleteSchool("TEST3");
+		DBController.deleteSchool("TEST4");
+		DBController.deleteSchool("TEST5");
+	}
+	@Test
+	public void testFindRelatedUniversities1() {
+		ArrayList<University> unis = search.findRelatedUniversities(test, 6);
+		assertTrue(unis.get(0).equals(test) && unis.get(1).equals(test5) && unis.get(2).equals(test1) && unis.get(3).equals(test2) && unis.get(4).equals(test3) && unis.get(5).equals(test4));
 	}
 	
-//	@Test
-//	public void testFindRelatedUniversities() {
-//		fail("Not yet implemented");
-//	}
-//
+	
+	@Test
+	public void testFindNRelatedUniversities1() {
+		assertTrue(search.findRelatedUniversities(test, 1).size() == 1);
+	}
+	@Test
+	public void testFindNRelatedUniversities2() {
+		assertTrue(search.findRelatedUniversities(test, 2).size() == 2);
+	}
+	@Test
+	public void testFindNRelatedUniversities5() {
+		assertTrue(search.findRelatedUniversities(test, 5).size() == 5);
+	}
+	
+	
+
 //	@Test
 //	public void testGetUniversity() {
 //		fail("Not yet implemented");
@@ -399,6 +437,107 @@ public class SearchControllerTest {
 	@Test
 	public void testSearchEnrolledFailMid() {
 		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 60, -1, -1, -1, -1, -1, -1, null);
+		assertFalse(search.getSearchedUniversities().contains(test));
+	}
+	@Test
+	public void testSearchAcademicdPassLower() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, null);
+		assertTrue(search.getSearchedUniversities().contains(test));
+	}
+	
+	@Test
+	public void testSearchAcademicPassUpper() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 3, -1, -1, -1, -1, null);
+		assertTrue(search.getSearchedUniversities().contains(test));
+	}
+	@Test
+	public void testSearchAcademicPassMid() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 2, -1, -1, -1, -1, null);
+		assertTrue(search.getSearchedUniversities().contains(test));
+	}
+	
+	@Test
+	public void testSearchAcademicFailLower() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 3, -1, -1, -1, -1, -1, null);
+		assertFalse(search.getSearchedUniversities().contains(test));
+	}
+	
+	@Test
+	public void testSearchAcademicFailUpper() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, null);
+		assertFalse(search.getSearchedUniversities().contains(test));
+	}
+	@Test
+	public void testSearchacAcademicFailMid() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 4, 5, -1, -1, -1, -1, null);
+		assertFalse(search.getSearchedUniversities().contains(test));
+	}
+	
+	@Test
+	public void testSearchSocialPassLower() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, null);
+		assertTrue(search.getSearchedUniversities().contains(test));
+	}
+	
+	@Test
+	public void testSearchSocialPassUpper() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 3, -1, -1, null);
+		assertTrue(search.getSearchedUniversities().contains(test));
+	}
+	@Test
+	public void testSearchSocialPassMid() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 2, -1, -1, null);
+		assertTrue(search.getSearchedUniversities().contains(test));
+	}
+	
+	@Test
+	public void testSearchSocialFailLower() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 3, -1, -1, -1, null);
+		assertFalse(search.getSearchedUniversities().contains(test));
+	}
+	
+	@Test
+	public void testSearchSocialFailUpper() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, null);
+		assertFalse(search.getSearchedUniversities().contains(test));
+	}
+	@Test
+	public void testSearchacSocialFailMid() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, 4, 5, -1, -1, null);
+		assertFalse(search.getSearchedUniversities().contains(test));
+	}
+	
+	@Test
+	public void testSearchLifePassLower() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, -1, null);
+		assertTrue(search.getSearchedUniversities().contains(test));
+	}
+	
+	@Test
+	public void testSearchLifePassUpper() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 3, null);
+		assertTrue(search.getSearchedUniversities().contains(test));
+	}
+	@Test
+	public void testSearchLifePassMid() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 2, null);
+		assertTrue(search.getSearchedUniversities().contains(test));
+	}
+	
+	@Test
+	public void testSearchLifeFailLower() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 3, -1, null);
+		assertFalse(search.getSearchedUniversities().contains(test));
+	}
+	
+	@Test
+	public void testSearchLifeFailUpper() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, null);
+		assertFalse(search.getSearchedUniversities().contains(test));
+	}
+	@Test
+	public void testSearchacLifeFailMid() {
+		search.search(null, null, null, null, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, 4, 5, null);
 		assertFalse(search.getSearchedUniversities().contains(test));
 	}
 }
