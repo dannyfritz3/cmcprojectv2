c9c0b59d13ff404afcd2ac2fc7f1af9cac226386
k1becker <k1becker@viewlincsci37.ad.csbsju.edu>
2017-04-04 19:22:10 -0500

    Account testin'

--- tests/baseclasses/AccountTest.java
+++ tests/baseclasses/AccountTest.java
@@ -7,7 +7,22 @@ import org.junit.Test;
 import java.util.*;
 
 public class AccountTest {
-
+	public Account user1, user2, user3;
+	@Before
+	public void setup()
+	{
+		user1 = new Account("dannyf", "Danny", "Fritz", "password", 'u', 'Y');
+		user2 = new Account("dannyf", "Danny", "Fritz", "password", 'u', 'Y');
+		user3 = new Account("dannyd", "Danny", "Fritz", "password", 'u', 'Y');
+	}
 	
-
+	@Test
+	public void testAccountEqualMethod() {
+		assertTrue(user1.equals(user2));
+	}
+	
+	@Test
+	public void testAccountNotEqualMethod() {
+		assertFalse(user1.equals(user3));
+	}
 }
